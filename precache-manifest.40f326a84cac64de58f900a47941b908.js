7ddbc1714946f6a887eb0f632735bbbad6f24e81
Vivek-Kamble <vvek.kamble@gmail.com>
2020-06-17 20:11:21 +0530

    final

---
+++ precache-manifest.40f326a84cac64de58f900a47941b908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4003b30bc98eca4316a2bfa4e2bc5421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a374fc5af94c58a1a86a",
+    "url": "/static/css/main.ad528ef3.chunk.css"
+  },
+  {
+    "revision": "d222bf11ce8d31a1ad4f",
+    "url": "/static/js/2.bcd433fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bcd433fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a374fc5af94c58a1a86a",
+    "url": "/static/js/main.931ff38c.chunk.js"
+  },
+  {
+    "revision": "11b21284da0ffe7542af",
+    "url": "/static/js/runtime-main.8e685992.js"
+  },
+  {
+    "revision": "11da2c66f8b9ae430638749bc648e673",
+    "url": "/static/media/login.11da2c66.png"
+  }
+]);
